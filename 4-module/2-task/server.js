7008ed447f97028db685864c8546d5acd398ea5c
ilya_zelenskiy <ilya.zelenskiy@vaz.ru>
2020-04-02 13:38:56 +0400

    add solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,23 +1,65 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const limitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+    const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('not allowed');
+        return;
+    }
 
-  switch (req.method) {
-    case 'POST':
+    const filepath = path.join(__dirname, 'files', pathname);
 
-      break;
+    switch (req.method) {
+        case 'POST':
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+            const file = fs.createWriteStream(filepath, {flags: 'wx'});
+            const limitStream = new limitSizeStream({limit: 1e6});
+
+            file.on('error', err => {
+                if (err.code === 'EEXIST') {
+                    res.statusCode = 409;
+                    res.end('file already exists')
+                }
+
+                res.statusCode = 500;
+                res.end('something wrong');
+            });
+
+            limitStream.on('error', err => {
+              if(err.code === 'LIMIT_EXCEEDED') {
+                res.statusCode = 413;
+                res.end('file is too big');
+
+                fs.unlink(filepath, () => {});
+                return;
+              }
+            });
+
+            req.pipe(limitStream).pipe(file);
+
+            file.on('close', () => {
+              res.statusCode = 201;
+              res.end('saved');
+            });
+
+            req.on('aborted', () => {
+                fs.unlink(filepath, () => {});
+            });
+
+            break;
+
+        default:
+            res.statusCode = 501;
+            res.end('Not implemented');
+    }
 });
 
 module.exports = server;
--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,33 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
+      if(pathname.indexOf('\/') >= 0) {
+        res.statusCode = 400;
+        res.end('Path not available');
+      } else {
+        fs.unlink(filepath, err => {
+          if(!err) {
+            return res.end('ok');
+          }
+          if(err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('file not found');
+          } else {
+            res.statusCode = 500;
+            res.end('internal server error');
+          }
+        });
+      }
       break;
 
     default:
