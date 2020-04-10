3547e80140b172094f74325da2910c5182939854
ilya_zelenskiy <ilya.zelenskiy@vaz.ru>
2020-04-10 21:58:03 +0400

    add solutions for 6-module 3 task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,5 @@
+const Product = require('../models/Product');
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  let prod = await Product.find({$text: {$search: ctx.query.query}});
+  ctx.body = {products: prod};
 };
