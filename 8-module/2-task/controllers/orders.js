cbe39687837018a2126be29557a4e53055363b8a
ilya_zelenskiy <ilya.zelenskiy@vaz.ru>
2020-04-21 16:13:56 +0400

    add solutions 8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -2,7 +2,29 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const order = await Order.create({
+        user: ctx.user,
+        product: ctx.request.body.product,
+        address: ctx.request.body.address,
+        phone: ctx.request.body.phone
+    });
+
+    await sendMail({
+        template: 'order-confirmation',
+        locals: {
+            id: order._id,
+            product: order.product._id
+        },
+        to: ctx.user.email,
+        subject: 'Ваш заказ'
+    });
+    ctx.body = {order: order._id};
+    return next();
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+    const orders = await Order.find({user: ctx.user.id});
+    ctx.status = 200;
+    ctx.body = {orders: orders};
+    return next();
 };
